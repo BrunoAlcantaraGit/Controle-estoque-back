299fea177a26a3194f2b4fc4b198b55d2f64d6be
BrunoAlcantara <brunodeoliveiraalcantara@gmail.com>
2025-01-04 20:31:19 -0300

    Criação da Variável para mudar porta padrão

--- src/main/java/com/controle/estoque/EstoqueApplication.java
+++ src/main/java/com/controle/estoque/EstoqueApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class EstoqueApplication {
-
+	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstoqueApplication.class, args);
