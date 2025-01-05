182a0e44e050a61810bd6392655174f43a35c2f3
BrunoAlcantara <brunodeoliveiraalcantara@gmail.com>
2025-01-04 21:32:27 -0300

    Correção erro de CROSS railway

--- src/main/java/com/controle/estoque/EstoqueApplication.java
+++ src/main/java/com/controle/estoque/EstoqueApplication.java
@@ -1,11 +1,15 @@
 package com.controle.estoque;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class EstoqueApplication {
 	public String PORT = System.getenv("PORT");
 
