34165932a478d8b7340f519632fae98d6d1dd373
BrunoAlcantara <brunodeoliveiraalcantara@gmail.com>
2025-01-11 16:48:37 -0300

    Criação de configuração de ambiente de produção e dev

--- src/main/java/com/controle/estoque/configuration/WebConfig.java
+++ src/main/java/com/controle/estoque/configuration/WebConfig.java
@@ -4,18 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://controle-de-estoque-216f4.web.app")
-
-                //http://localhost:4200/
-                //https://controle-de-estoque-216f4.web.app
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins(
+                        "http://localhost:4200", // URL do front-end local
+                        "https://controle-de-estoque-216f4.web.app" // URL do front-end em produção
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos os cabeçalhos
+                .allowCredentials(true); // Permitir envio de cookies, se necessário
     }
 }
